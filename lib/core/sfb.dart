ad191ba956db347ae29536c24d60dc0faa869495
Krzysztof Domanski <krzysiekdomanski@hotmail.com>
2025-10-24 17:45:38 +0100

    Added app theme with FlexColorTheme package

--- lib/core/sfb.dart
+++ lib/core/sfb.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/widget_previews.dart';
 import 'package:flutter/material.dart';
+import 'package:sfb/core/theme/app_theme.dart';
 
 class Sfb extends StatelessWidget {
+  @Preview(name: 'Sfb App')
   const Sfb({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      home: const Scaffold(
+        body: Center(child: Column(children: [Text('Hello World!')])),
+      ),
     );
   }
 }
