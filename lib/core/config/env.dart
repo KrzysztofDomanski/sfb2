6c5fe3e72225bdc6932f220ac07db839dfbe23d7
Krzysztof Domanski <krzysiekdomanski@hotmail.com>
2025-10-25 10:12:50 +0100

    SFB-3: Added supabase_flutter package and add supabase environment variables

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,5 +1,15 @@
 import 'package:envied/envied.dart';
 
+part 'env.g.dart';
+
 /// Configuration class for environment variables.
-@Envied()
-abstract class Env {}
+@Envied(requireEnvFile: true)
+abstract class Env {
+  /// Supabase project URL.
+  @EnviedField(obfuscate: true, varName: 'SUPABASE_URL')
+  static final String supabaseUrl = _Env.supabaseUrl;
+
+  /// Supabase project publishable key.
+  @EnviedField(obfuscate: true, varName: 'SUPABASE_PUB_KEY')
+  static final String supabasePubKey = _Env.supabasePubKey;
+}
